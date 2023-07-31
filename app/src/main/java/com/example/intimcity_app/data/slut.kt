c31398f8b327ac35a47097afeb039026b9c83221
nick-104 <f0r_c0d3@mail.ru>
2023-07-31 12:26:59 +0300

    slut class

---
+++ app/src/main/java/com/example/intimcity_app/data/slut.kt
@@ -0,0 +1,27 @@
+package com.example.intimcity_app.data
+
+class Slut(
+    subway: String,
+    age: Int,
+    tits: Int,
+    height: Int,
+    weight: Int,
+    hour_1: Int,
+    hour_2: Int,
+    night: Int,
+    description: String,
+    hasAnal: Boolean,
+    analCondition: String
+) {
+    val subway = subway
+    val age = age
+    val tits = tits
+    val height = height
+    val weight = weight
+    val hour_1 = hour_1
+    val hour_2 = hour_2
+    val night = night
+    val description = description
+    val hasAnal = hasAnal
+    val analCondition = analCondition
+}
